5cf20a77941d310f9c1ccc9c5524154b509b44ae
João Vitor Cleto <119801314+JohnnyCleto@users.noreply.github.com>
2024-11-28 17:33:13 -0300

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -46,40 +46,40 @@ const Home = () => {
       <main>
         <div className="promocao_lugares">
           <h1>Promoções de Serviços</h1>
-          <img className="img1" src="img/promocao1.jpg" alt="alongamento_unha" />
-          <img className="img2" src="img/promocao2.jpg" alt="progressiva" />
-          <img className="img3" src="img/promocao3.jpeg" alt="corte" />
-          <img className="img4" src="img/promoçao4.jpg" alt="tranças" />
-          <img className="img5" src="img/promocao5.jpeg" alt="massagem" />
+          <img className="img1" src="../img/promocao1.jpg" alt="alongamento_unha" />
+          <img className="img2" src="../img/promocao2.jpg" alt="progressiva" />
+          <img className="img3" src="../img/promocao3.jpeg" alt="corte" />
+          <img className="img4" src="../img/promoçao4.jpg" alt="tranças" />
+          <img className="img5" src="../img/promocao5.jpeg" alt="massagem" />
         </div>
 
         <div className="promocao_produtos">
           <h1>Promoções de Produtos</h1>
-          <img className="img1" src="img/promocao_produto1.jpg" alt="" />
-          <img className="img2" src="img/promocao_produto2.jpg" alt="" />
-          <img className="img3" src="img/promocao_produto3.jpg" alt="" />
-          <img className="img4" src="img/promoçao_produto4" alt="" />
-          <img className="img5" src="img/promocao_produto5." alt="" />
+          <img className="img1" src="../img/promocao_produto1.jpg" alt="" />
+          <img className="img2" src="../img/promocao_produto2.jpg" alt="" />
+          <img className="img3" src="../img/promocao_produto3.jpg" alt="" />
+          <img className="img4" src="../img/promoçao_produto4" alt="" />
+          <img className="img5" src="../img/promocao_produto5." alt="" />
         </div>
 
         <div className="logos_empresas">
           <h1>Lojas em destaque</h1>
-          <img className="img1" src="img/loja1.jpg" alt="" />
-          <img className="img2" src="img/loja2.jpg" alt="" />
-          <img className="img3" src="img/loja3.jpg" alt="" />
-          <img className="img4" src="img/loja4.jpg" alt="" />
-          <img className="img5" src="img/loja5.jpg" alt="" />
-          <img className="img6" src="img/loja6.jpg" alt="" />
-          <img className="img7" src="img/loja7.jpg" alt="" />
+          <img className="img1" src="../img/loja1.jpg" alt="" />
+          <img className="img2" src="../img/loja2.jpg" alt="" />
+          <img className="img3" src="../img/loja3.jpg" alt="" />
+          <img className="img4" src="../img/loja4.jpg" alt="" />
+          <img className="img5" src="../img/loja5.jpg" alt="" />
+          <img className="img6" src="../img/loja6.jpg" alt="" />
+          <img className="img7" src="../img/loja7.jpg" alt="" />
         </div>
 
         <div className="voce_sabia">
           <h1>Você sabia...?</h1>
-          <img className="img1" src="img/voce_sabia1.jpg" alt="" />
-          <img className="img2" src="img/voce_sabia2.jpg" alt="" />
-          <img className="img3" src="img/voce_sabia3.jpg" alt="" />
-          <img className="img4" src="img/voce_sabia4.jpg" alt="" />
-          <img className="img5" src="img/voce_sabia5.jpg" alt="" />
+          <img className="img1" src="../img/voce_sabia1.jpg" alt="" />
+          <img className="img2" src="../img/voce_sabia2.jpg" alt="" />
+          <img className="img3" src="../img/voce_sabia3.jpg" alt="" />
+          <img className="img4" src="../img/voce_sabia4.jpg" alt="" />
+          <img className="img5" src="../img/voce_sabia5.jpg" alt="" />
         </div>
       </main>
 

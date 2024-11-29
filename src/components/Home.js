05eef518454aaa985c9a483e2c6dc5ee988ee58b
João Vitor Cleto <119801314+JohnnyCleto@users.noreply.github.com>
2024-11-29 01:03:35 -0300

    Reapply "atualização de sara"
    This reverts commit 9f3325e9018287283b1be1056df109a0c22e3077.

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,19 +14,21 @@ const Home = () => {
     <div>
       <header>
         <div className="logocontainer">
-          <img
-            id="logoLink"
-            className="img_logo"
-            src="img/new_logo.jpg"
-            alt="Logo"
-          />
+        <img
+          id="logoLink"
+          className="img_logo"
+          src={require('./img/logo_fio_roxo.jpg')}
+          alt="Logo"
+        />
+
         </div>
         <span className="tres" onClick={abrirMenu}>
           &#9776;
         </span>
         <div className="perfil">
           <a href="perfil.html" id="perfilLink">
-            <img className="img_perfil" src="img/perfil.eu.jpg" alt="eu" />
+            <img className="img_perfil" src={require('./img/perfil.eu.jpg')} alt="eu" />
+
           </a>
         </div>
         <button className="botao_perfil">Seu perfil</button>
@@ -46,40 +48,40 @@ const Home = () => {
       <main>
         <div className="promocao_lugares">
           <h1>Promoções de Serviços</h1>
-          <img className="img1" src="../img/promocao1.jpg" alt="alongamento_unha" />
-          <img className="img2" src="../img/promocao2.jpg" alt="progressiva" />
-          <img className="img3" src="../img/promocao3.jpeg" alt="corte" />
-          <img className="img4" src="../img/promoçao4.jpg" alt="tranças" />
-          <img className="img5" src="../img/promocao5.jpeg" alt="massagem" />
+          <img className="img1" src={require('./img/promocao1.jpg')} alt="alongamento_unha" />
+          <img className="img2" src={require('./img/promocao2.jpg')} alt="progressiva" />
+          <img className="img3" src={require('./img/promocao3.jpeg')} alt="corte" />
+          <img className="img4" src={require('./img/promoçao4.jpg')} alt="tranças" />
+          <img className="img5" src={require('./img/promocao5.jpeg')} alt="massagem" />
         </div>
 
         <div className="promocao_produtos">
           <h1>Promoções de Produtos</h1>
-          <img className="img1" src="../img/promocao_produto1.jpg" alt="" />
-          <img className="img2" src="../img/promocao_produto2.jpg" alt="" />
-          <img className="img3" src="../img/promocao_produto3.jpg" alt="" />
-          <img className="img4" src="../img/promoçao_produto4" alt="" />
-          <img className="img5" src="../img/promocao_produto5." alt="" />
+          <img className="img1" src={require('./img/promocao_produto1.jpg')} alt="" />
+          <img className="img2" src={require('./img/promocao_produto2.jpg')} alt="" />
+          <img className="img3" src={require('./img/promocao_produto3.jpg')} alt="" />
+          <img className="img3" src={require('./img/Promocao_produto4.jpg')} alt="" />
         </div>
 
         <div className="logos_empresas">
           <h1>Lojas em destaque</h1>
-          <img className="img1" src="../img/loja1.jpg" alt="" />
-          <img className="img2" src="../img/loja2.jpg" alt="" />
-          <img className="img3" src="../img/loja3.jpg" alt="" />
-          <img className="img4" src="../img/loja4.jpg" alt="" />
-          <img className="img5" src="../img/loja5.jpg" alt="" />
-          <img className="img6" src="../img/loja6.jpg" alt="" />
-          <img className="img7" src="../img/loja7.jpg" alt="" />
+          <img className="img1" src={require('./img/loja1.jpg')} alt="" />
+          <img className="img2" src={require('./img/loja2.jpg')} alt="" />
+          <img className="img3" src={require('./img/loja3.jpg')} alt="" />
+          <img className="img4" src={require('./img/loja4.jpg')} alt="" />
+          <img className="img5" src={require('./img/loja5.jpg')} alt="" />
+          <img className="img6" src={require('./img/loja6.jpg')} alt="" />
+          <img className="img7" src={require('./img/loja7.jpg')} alt="" />
+
         </div>
 
         <div className="voce_sabia">
           <h1>Você sabia...?</h1>
-          <img className="img1" src="../img/voce_sabia1.jpg" alt="" />
-          <img className="img2" src="../img/voce_sabia2.jpg" alt="" />
-          <img className="img3" src="../img/voce_sabia3.jpg" alt="" />
-          <img className="img4" src="../img/voce_sabia4.jpg" alt="" />
-          <img className="img5" src="../img/voce_sabia5.jpg" alt="" />
+          <img className="img1" src={require('./img/voce_sabia1.jpg')} alt="" />
+          <img className="img2" src={require('./img/voce_sabia2.jpg')} alt="" />
+          <img className="img3" src={require('./img/voce_sabia3.jpg')} alt="" />
+          <img className="img4" src={require('./img/voce_sabia4.jpg')} alt="" />
+          <img className="img5" src={require('./img/voce_sabia5.jpg')} alt="" />
         </div>
       </main>
 

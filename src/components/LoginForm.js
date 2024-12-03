8b957d990a960bc36f4bdb4e52130bb30c683dbb
João Vitor Cleto <119801314+JohnnyCleto@users.noreply.github.com>
2024-12-03 13:20:55 -0300

    V6

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './LoginForm.css';
 
 const LoginForm = () => {
   const navigate = useNavigate();
@@ -11,20 +10,92 @@ const LoginForm = () => {
   const handleLogin = (e) => {
     e.preventDefault();
     if (email && password) {
-      navigate('/home'); // Redireciona para a página "Home"
+      navigate('/home');
     } else {
       alert('Por favor, preencha todos os campos.');
     }
   };
 
   const handleRegister = () => {
-    navigate('/cadastrar'); // Redireciona para a página "RegisterForm"
+    navigate('/cadastrar');
   };
 
+  const styles = {
+    global: {
+      fontFamily: 'arial, sans-serif',
+      margin: 0,
+      padding: 0,
+      boxSizing: 'border-box',
+    },
+    background_img: {
+      backgroundImage: "url('./img/background_img.jpg')",
+      minHeight: '100vh',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover',
+      display: 'flex',
+      position: 'relative',
+    },
+    formulario: {
+      position: 'absolute',
+      right: 0,
+      margin: '80px 10%',
+      maxWidth: '90%',
+      padding: '20px',
+      backgroundColor: 'white',
+      color: 'black',
+      border: '2px solid #6c4539',
+      borderRadius: '10px',
+    },
+    h1: {
+      marginBottom: '20px',
+    },
+    input: {
+      width: '100%',
+      padding: '15px',
+      margin: '5px 0 22px 0',
+      border: '1px solid #6c4539',
+      backgroundColor: '#ffecfc',
+      borderRadius: '50px',
+    },
+    inputFocus: {
+      backgroundColor: '#ffecfc',
+      outline: 'none',
+    },
+    esqueci_senha: {
+      fontSize: '20px',
+      color: '#f45bf4',
+      display: 'block',
+      textAlign: 'center',
+      marginBottom: '20px',
+      textDecoration: 'none',
+    },
+    esqueci_senhaHover: {
+      textDecoration: 'underline',
+      color: 'hsl(300, 99%, 64%)',
+    },
+    botao: {
+      backgroundColor: '#f45bf4',
+      color: 'white',
+      padding: '15px 20px',
+      border: '2px solid #6c4539',
+      cursor: 'pointer',
+      width: '60%',
+      opacity: 0.8,
+      fontSize: '20px',
+      marginLeft: '20%',
+      borderRadius: '50px',
+    },
+    botaoHover: {
+      opacity: 1,
+      backgroundColor: 'hsl(300, 99%, 64%)',
+    },
+  };
+  
   return (
-    <div className="background_img">
-      <form className="formulario" onSubmit={handleLogin}>
-        <h1>Login</h1>
+    <div style={styles.background_img}>
+      <form style={styles.formulario} onSubmit={handleLogin}>
+        <h1 style={styles.h1}>Login</h1>
         <label htmlFor="email"><b>Email</b></label>
         <input
           type="text"
@@ -33,6 +104,7 @@ const LoginForm = () => {
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           required
+          style={styles.input}
         />
         <label htmlFor="password"><b>Senha</b></label>
         <input
@@ -42,17 +114,41 @@ const LoginForm = () => {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           required
+          style={styles.input}
         />
         <div>
-          <a href="#" className="esqueci_senha">Esqueci minha senha</a>
+          <a
+            href="#"
+            style={styles.esqueci_senha}
+            onMouseOver={(e) => (e.target.style = styles.esqueci_senhaHover)}
+            onMouseOut={(e) => (e.target.style = styles.esqueci_senha)}
+          >
+            Esqueci minha senha
+          </a>
         </div>
-        <button type="submit" className="botao_login">Entrar</button>
-        <button type="button" className="botao_cadastrar" onClick={handleRegister}>
+        <button
+          type="submit"
+          style={styles.botao}
+          onMouseOver={(e) => (e.target.style = styles.botaoHover)}
+          onMouseOut={(e) => (e.target.style = styles.botao)}
+        >
+          Entrar
+        </button>
+        <button
+          type="button"
+          style={styles.botao}
+          onClick={handleRegister}
+          onMouseOver={(e) => (e.target.style = styles.botaoHover)}
+          onMouseOut={(e) => (e.target.style = styles.botao)}
+        >
           Cadastrar
         </button>
       </form>
     </div>
   );
+
+
+
 };
 
 export default LoginForm;
--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,39 +1,149 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';  // Importação correta do useNavigate
-import './RegisterForm.css';
+import { useNavigate } from 'react-router-dom'; // Importação correta do useNavigate
+
+const styles = {
+  global: {
+    fontFamily: 'Arial, sans-serif',
+    margin: '0 auto',
+    padding: 0,
+    boxSizing: 'border-box',
+  },
+  backgroundImg: {
+    backgroundImage: "url('./img/background_img.jpg')",
+    minHeight: '100%',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    position: 'relative',
+    display: 'flex',
+  },
+  formulario: {
+    position: 'absolute',
+    right: 0,
+    margin: '35px 150px 35px 35px',
+    maxWidth: '500px',
+    padding: '20px',
+    backgroundColor: 'white',
+    color: 'black',
+    border: '2px solid #6c4539',
+  },
+  h1: {
+    marginBottom: '10px',
+  },
+  input: {
+    width: '100%',
+    padding: '15px',
+    margin: '5px 0 10px 0',
+    border: '1px solid #6c4539',
+    backgroundColor: '#ffecfc',
+    borderRadius: '50px',
+  },
+  inputFocus: {
+    backgroundColor: '#ffecfc',
+    outline: 'none',
+  },
+  checkbox: {
+    color: '#f45bf4',
+    fontStyle: 'none',
+    display: 'block',
+    textAlign: 'center',
+    textDecoration: 'none',
+  },
+  checkboxInput: {
+    marginTop: '10px',
+    marginRight: '2px',
+  },
+  checkboxLabel: {
+    fontSize: '15px',
+  },
+  checkboxLink: {
+    color: '#f45bf4',
+    textDecoration: 'none',
+  },
+  checkboxLinkHover: {
+    textDecoration: 'underline',
+    color: 'hsl(300, 99%, 64%)',
+  },
+  botaoCadastrar: {
+    backgroundColor: '#f45bf4',
+    color: 'white',
+    padding: '15px 20px',
+    border: '2px solid #6c4539',
+    cursor: 'pointer',
+    width: '60%',
+    opacity: 0.8,
+    fontSize: '20px',
+    marginTop: '15px',
+    marginLeft: '90px',
+    borderRadius: '50px',
+  },
+  botaoCadastrarHover: {
+    opacity: 1,
+    backgroundColor: 'hsl(300, 99%, 64%)',
+  },
+};
 
 const RegisterForm = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="background_img">
-      <form className="formulario">
-        <h1>Cadastro</h1>
-        
+    <div style={styles.backgroundImg}>
+      <form style={styles.formulario}>
+        <h1 style={styles.h1}>Cadastro</h1>
+
         <label htmlFor="nome"><b>Nome</b></label>
-        <input type="text" placeholder="Digite seu nome" name="nome" required />
+        <input
+          type="text"
+          placeholder="Digite seu nome"
+          name="nome"
+          required
+          style={styles.input}
+        />
 
         <label htmlFor="email"><b>E-mail</b></label>
-        <input type="text" placeholder="Digite seu e-mail" name="email" required />
+        <input
+          type="text"
+          placeholder="Digite seu e-mail"
+          name="email"
+          required
+          style={styles.input}
+        />
 
         <label htmlFor="psw"><b>Senha</b></label>
-        <input type="password" placeholder="Digite sua senha" name="psw" required />
+        <input
+          type="password"
+          placeholder="Digite sua senha"
+          name="psw"
+          required
+          style={styles.input}
+        />
 
         <label htmlFor="psw-confirm"><b>Confirmar Senha</b></label>
-        <input type="password" placeholder="Confirme sua senha" name="psw-confirm" required />
+        <input
+          type="password"
+          placeholder="Confirme sua senha"
+          name="psw-confirm"
+          required
+          style={styles.input}
+        />
 
-        <div className="checkbox">
-          <input type="checkbox" id="concordo" name="concordo" required />
-          <label htmlFor="concordo">
-            Li e concordo com os <a href="#">termos de uso</a>
+        <div style={styles.checkbox}>
+          <input
+            type="checkbox"
+            id="concordo"
+            name="concordo"
+            required
+            style={styles.checkboxInput}
+          />
+          <label htmlFor="concordo" style={styles.checkboxLabel}>
+            Li e concordo com os <a href="#" style={styles.checkboxLink}>termos de uso</a>
           </label>
         </div>
 
-        {/* Botão para cadastrar e redirecionar para a página inicial */}
         <button
           type="button"
-          className="botao_cadastrar"
-          onClick={() => navigate('/home')} // Ajuste na rota
+          style={styles.botaoCadastrar}
+          onClick={() => navigate('/home')}
         >
           Cadastrar
         </button>
